73d7f18adb91dbf49ab845636b8cbe92fe429e10
Shivam <upshivam01@gmail.com>
2021-12-05 14:20:28 +0530

    There are two clocks created by me using JavaScript. First one is Analog clock and second one is Digital clock.

---
+++ Analog Clock/index.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+    date = new Date();
+    htime = date.getHours();
+    mtime = date.getMinutes();
+    stime = date.getSeconds();
+
+    hrotation = 30*htime + mtime/2 + stime/120;
+    mrotation = 6*mtime + stime/10;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
